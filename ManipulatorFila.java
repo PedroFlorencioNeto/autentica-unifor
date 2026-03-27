32f0ac8914017127b08ea4fb2bf8d4974fb71018
Pedro Florencio <pedroflorencio@MacBook-Air-de-Pedro.local>
2026-03-26 21:04:48 -0300

    feat: add some notes

---
+++ ManipulatorFila.java
@@ -0,0 +1,8 @@
+public class ManipulatorFila {
+    Fila criar(){
+        Fila f = new Fila();
+        f.inicio = null;
+        f.fim = null;
+        return f;
+    }
+}
---
+++ Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args) {
+        ManipulatorFila m = new ManipulatorFila();
+        Fila f = m.criar();
+        System.out.println(f.inicio);
+        System.out.println(f.fim);
+    }
+}
